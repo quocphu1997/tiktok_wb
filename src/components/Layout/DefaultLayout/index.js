06c1de016c6d00a61a1ea44a5be5025d4872349a
quocphu1997 <congaquevip@gmail.com>
2022-08-02 14:37:56 +0700

    upload

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+
+export default function DefaultLayout({children}) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
